a6f4ea23bbcfe31acb8bd665030eb87d193ef152
Karl Labrador <karl.labrad@gmail.com>
2022-03-24 14:52:43 +0100

    add rest_framework to app

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Other
+    'rest_framework',
+    # aclgen
 ]
 
 MIDDLEWARE = [
