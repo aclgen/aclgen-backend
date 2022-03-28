92fd7d8b17429b85588917fed6459bd2d7f9a60d
Karl Labrador <karl.labrad@gmail.com>
2022-03-28 14:52:34 +0200

    added names to object url paths

--- app/object/urls.py
+++ app/object/urls.py
@@ -3,6 +3,6 @@ from .views import ObjectViewSet
 from .views import ObjectDetailViewSet
 
 urlpatterns = [
-    path('', ObjectViewSet.as_view()),
-    path('<int:object_id>', ObjectDetailViewSet.as_view())
+    path('', ObjectViewSet.as_view(), name="object"),
+    path('<int:object_id>', ObjectDetailViewSet.as_view(), name="object_with_id")
 ]
