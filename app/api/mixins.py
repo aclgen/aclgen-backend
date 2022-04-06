ec13cde84fbefb57f30af1a7cf357059f91ea42a
Karl Labrador <karl.labrad@gmail.com>
2022-04-06 11:03:56 +0200

    Fix RepositoryLinkMixin generating collision errors

--- app/api/mixins.py
+++ app/api/mixins.py
@@ -3,8 +3,7 @@ from app.api.models.repository import Repository
 
 
 class RepositoryLinkMixin(models.Model):
-    repository = models.ForeignKey(Repository, on_delete=models.CASCADE, blank=False, null=False,
-                                   related_name="devices")
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE, blank=False, null=False)
 
     class Meta:
         abstract = True
