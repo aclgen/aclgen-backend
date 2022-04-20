88b2ef40114262f144b763f6747ab4313db83858
Karl Labrador <karl.labrad@gmail.com>
2022-04-20 03:26:44 +0200

    Cleanup ServiceViewSet (using the mixins now)

--- app/api/views/service.py
+++ app/api/views/service.py
@@ -16,35 +16,20 @@ class ServiceViewSet(
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
 
-    def _get_values(self, with_service_id=True):
-        if with_service_id:
-            return self.kwargs.get("repo_id"), self.kwargs.get("service_id")
-        return self.kwargs.get("repo_id")
+    def get_queryset(self):
+        repository = self.kwargs.get("repo_id")
+        service = self.kwargs.get("service_id")
+
+        if repository and service:
+            return self.queryset.filter(repository=repository, id=service)
+
+        if repository and not service:
+            return self.queryset.filter(repository=repository)
+
+        return self.queryset
 
     def get_object(self):
-        repo_id, service_id = self._get_values()
-        return get_object_or_404(self.queryset, id=service_id, repository=repo_id)
-
-    def create(self, request, *args, **kwargs):
-        repo_id = self._get_values(with_service_id=False)
-
-        data = request.data | {
-            "repository": repo_id
-        }
-
-        serializer = ServiceSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def list(self, request, *args, **kwargs):
-        repo_id = self._get_values(with_service_id=False)
-        instances = self.queryset.filter(repository=repo_id)
-        serialized = ServiceSerializer(instances, many=True)
-
-        return Response(serialized.data, status=status.HTTP_200_OK)
+        return get_object_or_404(self.get_queryset())
 
 
 class ServiceBulkViewSet(
