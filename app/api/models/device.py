810733eb3dc2631ba0eca99bb80d7db06ede5947
Karl Labrador <karl.labrad@gmail.com>
2022-04-28 06:18:56 +0200

    Removed RepositoryLinkMixin from dev, obj and service models

--- app/api/models/device.py
+++ app/api/models/device.py
@@ -1,15 +1,17 @@
 from django.db import models
 from app.util.models import BaseModel
 from app.common.mixins import UUIDPrimarySelfMixin
-from app.api.mixins import RepositoryLinkMixin, StatusFieldMixin
+from app.api.mixins import StatusFieldMixin
 from app.api.enums import DeviceType
+from app.api.models.repository import Repository
 
 
-class Device(UUIDPrimarySelfMixin, RepositoryLinkMixin, BaseModel, StatusFieldMixin):
+class Device(UUIDPrimarySelfMixin, BaseModel, StatusFieldMixin):
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE, blank=False, null=False,
+                                   related_name="devices")
     name = models.TextField(max_length=64)
     comment = models.TextField(max_length=255)
     type = models.CharField(max_length=64, choices=DeviceType.choices(), default=DeviceType.FIREWALL)
-    # TODO: Add other Device Specific fields?
 
     class Meta:
         verbose_name = "Device"
--- app/api/models/object.py
+++ app/api/models/object.py
@@ -1,10 +1,13 @@
 from django.db import models
 from app.util.models import BaseModel
 from app.common.mixins import UUIDPrimarySelfMixin
-from app.api.mixins import RepositoryLinkMixin, StatusFieldMixin
+from app.api.mixins import StatusFieldMixin
+from app.api.models.repository import Repository
 
 
-class Object(UUIDPrimarySelfMixin, RepositoryLinkMixin, BaseModel, StatusFieldMixin):
+class Object(UUIDPrimarySelfMixin, BaseModel, StatusFieldMixin):
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE, blank=False, null=False,
+                                   related_name="objectlist")
     name = models.TextField(max_length=32)
     comment = models.TextField(max_length=255)
     range_start = models.TextField(max_length=64)
